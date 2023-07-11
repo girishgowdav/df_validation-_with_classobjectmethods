064a033784ca7cc97e1a9332e281b27dcdf45ae9
girishgowdav <gowdagirishv38@gmail.com>
2023-07-11 14:22:06 +0530

    first commit

---
+++ dv_classobjectmethods/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dv_classobjectmethods project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dv_classobjectmethods.settings')
+
+application = get_wsgi_application()
